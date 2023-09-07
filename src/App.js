727e45bcf425ccde9c81a37906390b89cd960a81
MahenderKr <mahendra55555555@gmail.com>
2023-09-07 20:37:23 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import AlbumList from './components/AlbumList';
 import AddAlbum from './components/AddAlbum';
 import EditAlbum from './components/EditAlbum';
 
-const API_URL = "https://jsonplaceholder.typicode.com/albums";
+const API_URL = "https://jsonplaceholder.typicode.com/albums/";
 
 const App = () => {
   const [isAdding, setIsAdding] = useState(false);
@@ -32,8 +32,8 @@ const App = () => {
   const addAlbumToList = async (userId, title) => {
     try {
       const response = await axios.post(API_URL, {
-       title: title,
-       userId: 1
+        title: title,
+        userId: 1
       });
       setAlbums([...albums, response.data])
     }
@@ -41,35 +41,60 @@ const App = () => {
       console.error("Error fetching album:", error);
     }
   }
- const handleAddAlbum = (trackId, title) => {
+  const handleAddAlbum = (trackId, title) => {
     addAlbumToList(trackId, title);
   }
 
 
   //method to set the album which will be edited
   const handleEdit = (id) => {
-    let tempAlbum = albums.filter(x=>x.id==id)
+
+    let tempAlbum = albums.filter(x => x.id == id)
     setAlbum(tempAlbum);
     setIsEditing(true);
   };
 
   //method to update the values of the album which was edited
-  const handleUpdate = (id, trackId, title) =>
-  {
-    let tempAlbums = albums;
-    tempAlbums.map((album)=>
-    {
-      if(album.id==id)
-      {
-        album.userId = trackId;
-        album.title = title;
+  const handleUpdate = async (id, trackId, title) => {
+    try {
+      let tempAlbums = [...albums];
+      let albumToEdit = await handlePutRequest(id, trackId, title);
+
+      // Find and update the album in the tempAlbums array
+      const index = tempAlbums.findIndex((album) => album.id === albumToEdit.id);
+      if (index !== -1) {
+        tempAlbums[index].userId = albumToEdit.userId;
+        tempAlbums[index].title = albumToEdit.title;
       }
-    });
-    setAlbums([...tempAlbums]);
+
+      setAlbums(tempAlbums);
+    } catch (error) {
+      console.error("Error updating album:", error);
+    }
   }
 
+  const handlePutRequest = async (id, trackId, title) => {
+    let response = await fetch(`${API_URL}${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        id: id,
+        title: title,
+        body: 'bar',
+        userId: trackId,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      }
+    }).then(response => response.json());
+
+    return response;
+  }
   //method to delete a record from a list as per the id
-  const handleDelete = id => {
+  const handleDelete = async (id) => {
+    let response = await fetch(`${API_URL}${id}`, {
+      method: 'DELETE',
+    }).then(response=>response.json());
+    //since delete request doesnt give any response deleting it seperately
     setAlbums(albums.filter(x => x.id != id))
   };
 
